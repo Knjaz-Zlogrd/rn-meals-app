a26e0c2f5433d7337e4fbe8ec008a3cd4a8fef7a
Mihajlo <mulic92zr@gmail.com>
2021-11-08 14:36:11 +0100

    Added 2 tabs on starting screen

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,10 +1,13 @@
 import { Platform } from "react-native";
 import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
 
 import { createAppContainer } from "react-navigation";
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoriyMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
+import FavoritesScreen from '../screens/FavoritesScreen';
+
 import Colors from '../constants/Colors';
 
 const MealsNavigator = createStackNavigator({
@@ -22,6 +25,12 @@ const MealsNavigator = createStackNavigator({
       },
       headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
     }
+  }
+);
+
+const MealsFavTabNavigator = createBottomTabNavigator({
+  Meals: MealsNavigator,
+  Favorites: FavoritesScreen
 });
 
-export default createAppContainer(MealsNavigator);
+export default createAppContainer(MealsFavTabNavigator);
