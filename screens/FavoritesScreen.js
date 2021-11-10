d5e272d6d7fdcfa3796e865e4a27f7852d9146cf
Mihajlo <mulic92zr@gmail.com>
2021-11-10 19:05:22 +0100

    Adding Reducers to project, not sure if it will work

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import HeaderButton from '../components/HeaderButton';
+import { useSelector } from "react-redux";
 
 import MealList from "../components/MealList";
-import { MEALS } from "../data/dummy-data";
 
 const FavoritesScreen = props => {
-  const favMeals = MEALS.filter(meal => meal.id === 'm1' || meal.id === 'm2'); //will replace this logic
+  const favMeals = useSelector(state => state.meals.meals)
+
+  //const favMeals = MEALS.filter(meal => meal.id === 'm1' || meal.id === 'm2'); //will replace this logic
   return (
     <MealList 
       listData={favMeals}
